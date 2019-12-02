5ad88948caede1458b780b330dbec85c49466878
Suraj Pai <surajballambat@gmail.com>
2019-12-02 17:57:02 +0100

    Add auction creation and pure auction

---
+++ auction_creator.py
@@ -0,0 +1,74 @@
+import numpy as np
+import logging
+
+class AuctionCreator:
+    def __init__(self):
+        self.K = 0
+        self.N = 0
+        self.R = 0
+        self.Smax = 0
+        self.epsilon = 0.0
+        self.auction_type = ['0: Pure', '1: Levelled Commitment']
+        
+    
+    def get_user_input(self):
+        for attribute in dir(self):
+            if type(getattr(self, attribute)) == int:
+                while True:
+                    input_value = input("Enter the value for {}: ".format(attribute))
+                    try: 
+                        input_value = int(input_value)
+                        if attribute == "N":
+                            if input_value < getattr(self, 'K'):
+                                raise ValueError('Buyer, N should be larger than sellers, K')
+                        break
+                    except ValueError as error:
+                        logging.error(error)
+                        logging.error("Invalid Input. Please enter an valid integer value.")
+                setattr(self, attribute, input_value)
+
+
+            elif type(getattr(self, attribute)) == float:
+                while True:
+                    input_value = input("Enter the value for {}: ".format(attribute))
+                    try: 
+                        input_value = float(input_value)
+                        if input_value < 0.0 or input_value > 1.0:
+                            raise ValueError('Epsilon value should be between 0 and 1')
+                        break
+                    except ValueError as error:
+                        logging.error(error)
+                        logging.error("Invalid Input. Please enter a valid float value within 0.0 - 1.0")
+                setattr(self, attribute, input_value)
+
+
+            elif type(getattr(self, attribute)) == list:
+                while True:
+                    input_value = input("Enter the value for {} by selecting the option index  \n {} :".format(attribute, getattr(self, attribute)))
+                    try: 
+                        input_value = int(input_value)
+                        if input_value not in [0, 1]:
+                            raise ValueError('The available choices are either 0 or 1')
+                        break
+                    except ValueError as error:
+                        logging.error(error)
+                        logging.error("Invalid Input. Please enter a valid option value.")
+                setattr(self, attribute, input_value)
+
+    def create_bid_matrices(self):
+        self.seller_price = np.random.rand(self.K, self.R) * self.Smax
+        self.alpha_factors = np.random.rand(self.N, self.K) + 1 #Greater than 1
+        self.buyer_price = np.zeros((self.N, self.K, self.R))
+        
+        number_of_rounds = self.seller_price.shape[1]
+        for round in range(number_of_rounds):
+            self.buyer_price[:, :, round] = self.alpha_factors * self.seller_price[:, round]
+
+        return self.seller_price, self.buyer_price
+        
+
+
+if __name__ == "__main__":
+    auction_input = AuctionCreator()
+    auction_input.get_user_input()  
+    auction_input.create_bid_matrices()          
---
+++ auction_simulator.py
@@ -0,0 +1,93 @@
+from auction_creator import AuctionCreator
+import logging
+import numpy as np 
+
+logging.basicConfig(level=None)
+
+class AuctionSimulator:
+    def __init__(self, seller_prices, buyer_prices, auction_type):
+        self.auction_type = auction_type
+        self.seller_prices = seller_prices
+        self.buyer_prices = buyer_prices
+        self.seller_profits = np.zeros((self.seller_prices.shape[0]))
+        self.buyer_profits = np.zeros((self.buyer_prices.shape[0]))
+        self.number_of_rounds = self.buyer_prices.shape[2]
+        self.number_of_auctions = self.buyer_prices.shape[1]
+        self.auction_results = {}
+    
+        
+
+
+    def run_auctions(self):
+        if self.auction_type == 0:
+            self.run_pure_auction()
+        else:
+            raise NotImplementedError()
+
+        
+    def run_pure_auction(self):
+        for round in range(self.number_of_rounds):
+            self.auction_results["Round {}".format(round)] = {}
+
+            for k in range(self.number_of_auctions):
+                self.auction_results["Round {}".format(round)]["Auction {}".format(k)] = {}
+
+                logging.info('Sk value for current auction: {}'.format(self.seller_prices[k, round]))
+                buyer_prices_for_auction = self.buyer_prices[:, k, round]
+                logging.info('Buyer Prices for the current auction: {}'.format(buyer_prices_for_auction))
+                market_price_for_auction = self.calculate_market_price(buyer_prices_for_auction)
+                auction_winner = self.get_auction_winner(market_price_for_auction, buyer_prices_for_auction)
+
+                if len(auction_winner) != 0:
+                    self.calculate_seller_profits(k, buyer_prices_for_auction[auction_winner[0]])
+                    self.calculate_buyer_profits(auction_winner[0], market_price_for_auction, buyer_prices_for_auction[auction_winner[0]])
+                logging.info("Buyer Profits: {}".format(self.buyer_profits))
+                logging.info("Seller Profits: {}".format(self.seller_profits))
+
+                self.auction_results["Round {}".format(round)]["Auction {}".format(k)]["Winner"] = auction_winner
+                self.auction_results["Round {}".format(round)]["Auction {}".format(k)]["Market Price"] = market_price_for_auction
+                self.auction_results["Round {}".format(round)]["Auction {}".format(k)]["Buyer Profits"] = self.buyer_profits
+                self.auction_results["Round {}".format(round)]["Auction {}".format(k)]["Seller Profits"] = self.seller_profits
+
+    def calculate_market_price(self, prices):
+        market_price = np.average(prices)
+        logging.info("Market Price for current auction: {}".format(market_price))
+        return market_price
+
+    def calculate_seller_profits(self, seller, profits):
+        self.seller_profits[seller] += profits
+
+    def calculate_buyer_profits(self, buyer, market_price, profits):
+        self.buyer_profits[buyer] += market_price - profits
+
+    def get_auction_winner(self, market_price, buyers_list):
+        prices_below_market = buyers_list < market_price
+        auction_winner = np.where(buyers_list == np.partition(prices_below_market * buyers_list, -2)[-2])[0]
+
+        ## Possible case where no auction winner exists because the only single value below the market price,
+        ## depends on the value of alpha
+        logging.info("Auction Winner is Buyer: {}".format(auction_winner))
+        return auction_winner
+
+
+def prettyprintdict(dictionary):
+    for key in dictionary:
+        print("\n")
+        print(key)
+        for key_level2 in dictionary[key]:
+            print("\n")
+            print(key_level2)
+            for key_level3 in dictionary[key][key_level2]:
+                print(key_level3, dictionary[key][key_level2][key_level3])
+
+        
+
+    
+if __name__ == "__main__":
+    auction_input = AuctionCreator()
+    auction_input.get_user_input()  
+    seller_prices, buyer_prices = auction_input.create_bid_matrices()  
+    auction_simulator = AuctionSimulator(seller_prices, buyer_prices, auction_input.auction_type)
+    auction_simulator.run_auctions()
+    prettyprintdict(auction_simulator.auction_results)
+
